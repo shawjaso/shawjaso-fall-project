391130465041562bc170e1ae653f208607c2aef1
Jason Shaw <shawjaso@uw.edu>
2011-11-29 04:57:01 -0800

    Preparing for merge with master

--- lib/db.rb
+++ lib/db.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'logger'
 
 # Loading the db info and establishing the a connection
-db_config_file = 'db_config.yml'
+db_config_file = 'database.yml'
 db_config = YAML::load(File.open(db_config_file))
 ActiveRecord::Base.establish_connection db_config
 
--- lib/users.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-
-  validates :owner_name, :owner_fb_id, :presence => true
-
-end
-
